db303137a6fc7630b21669a314e077005689b081
Victor-Bertalan <56827595+Victor-Bertalan@users.noreply.github.com>
2020-06-24 18:58:50 +0300

    text animation

---
+++ assets/js/textfade.js
@@ -0,0 +1,28 @@
+const text=document.querySelectorAll(".fancy");
+for(let i=0;i<text.length;i++){
+    strtext=text[i].textContent;
+    console.log(strtext);
+    const splittext=strtext.split(" ");
+    console.log(splittext);
+    text[i].textContent="";
+
+    for (let j=0; j<splittext.length;j++){
+    text[i].innerHTML+="<span>" + splittext[j]+" " +"</span>"; 
+    }  
+
+    let char=0;
+    let timer=setInterval(onTick, 1000/3);
+
+    function onTick(){
+    const span=text[i].querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char === splittext.length)
+    {
+        clearInterval(timer);
+        return;
+    }
+    }
+    
+}
+
